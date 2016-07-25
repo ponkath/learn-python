0588c832e6a62bd73ec1ed6e0b3a0a2812fe8ce6
Kathleen Streit <kat.madrid@gmail.com>
2016-07-25 23:06:18 +0100

    Fix exercise 4.7

--- computegrade.py
+++ computegrade.py
@@ -1,5 +1,9 @@
 #Score2 - Excercise 4.7 COMPUTEGRADE
 
+'''Exercise 4.7 Rewrite the grade program from the previous chapter using a function
+called computegrade that takes a score as its parameter and returns a grade
+as a string.'''
+
 """
 score = input('Enter score: ')
 scr = float(score)
@@ -19,22 +23,28 @@ else:
 	print ('Bad score!')
 """
 
-def computegrade(score):
-    if float(score) >0 and float(score) <=1:
-            if float(score) < 0.6:
-                    score = ('F')
-            elif float(score) <= 0.7:
-                    score = ('D')
-            elif float(score) <= 0.8:
-                    score = ('C')
-            elif float(score) <= 0.9:
-                    score = ('B')
-            elif float(score) <= 1.0:
-                    score = ('A')
-    else:
-            print ('Bad score!')
+def computegrade(score_input):
 
-score = input('Enter score: ')
-scr = float(score)
+    grade = 'Bad score!'
+    
+    try:
+        score = float(score_input)
+    except:
+        score = -1
 
-print ('Your score is: ' + str(computegrade(score)))
+    if score >0 and score <=1:
+            if score < 0.6:
+                    grade = ('F')
+            elif score <= 0.7:
+                    grade = ('D')
+            elif score <= 0.8:
+                    grade = ('C')
+            elif score <= 0.9:
+                    grade = ('B')
+            elif score <= 1.0:
+                    grade = ('A')
+    return grade
+
+
+score_str = input('Enter score: ')
+print ('Your score is: ' + computegrade(score_str))
