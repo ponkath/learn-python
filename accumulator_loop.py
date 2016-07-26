94e6fb7b4825d3ca3c074d6a242620134f4e3edc
Kathleen Streit <kat.madrid@gmail.com>
2016-07-26 21:12:56 +0100

    Loops

---
+++ accumulator_loop.py
@@ -0,0 +1,16 @@
+#accumulator definite loop
+
+total = 0
+for intervar in [3, 41, 12, 9, 74, 15]:
+    total = total + intervar
+print ('Total: ', total)
+
+"""
+#Test Gauss
+
+counter = 0
+for i in range(100):
+    counter = counter + 1
+    total = counter
+print ('The Gauss number is: ',total)
+"""
---
+++ summing_loop.py
@@ -0,0 +1,6 @@
+#counting and summing loops
+
+count = 0
+for intervar in [3, 41, 12, 9, 74, 15]:
+    count = count + 1
+print ('Count: ', count)
