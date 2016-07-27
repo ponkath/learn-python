6d6cd474f578f9194bfb6c807a4a547c554730d7
Kathleen Streit <kat.madrid@gmail.com>
2016-07-27 22:46:38 +0100

    Lecture 5.3 exercises

---
+++ none_idiom.py
@@ -0,0 +1,13 @@
+#loop: the smallest so far IDIOM, with None as type
+
+smallest = None
+print ('Before')
+for value in [9,41,12,3,74,15]:
+    if smallest is None:
+       smallest = value
+    elif value < smallest:
+       smallest = value
+    print (smallest,value)
+
+print (('After'),smallest)
+
