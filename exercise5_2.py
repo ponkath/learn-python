210131d90e0b275279cd0c7be7179fb0a42547f9
Kathleen Streit <kat.madrid@gmail.com>
2016-07-31 20:37:31 +0100

    Update 5.2

--- exercise5_2.py
+++ exercise5_2.py
@@ -1,13 +1,34 @@
 #Exercise 5.2
-"""Write a program which repeatedly reads numbers until the user enters
-“done”. Once “done” is entered, print out the total, count,  minimum and maximum of
-the numbers. If the user enters anything other than a number, detect their mistake
-using try and except and print an error message and skip to the next number."""
+"""5.2 Write a program that repeatedly prompts a user for integer numbers
+until the user enters 'done'. Once 'done' is entered, print out the largest
+and smallest of the numbers. If the user enters anything other than a valid
+number catch it with a try/except and put out an appropriate message and
+ignore the number. Enter the numbers from the book for problem 5.1 and Match
+the desired output as shown"""
+
+"""
+PYTHONv2
+smallest_so_far = None
+largest_so_far = None
+userinp = None
+while True:
+    userinp = raw_input('Enter a number: ')
+    try:
+        if userinp == 'done':
+            break
+        number = int(userinp)
+        if smallest_so_far == None or number < smallest_so_far:
+            smallest_so_far = number
+        if largest_so_far == None or number > largest_so_far:
+            largest_so_far = number
+    except:
+        print 'Invalid input'
+print 'Maximum is',largest_so_far
+print 'Minimum is', smallest_so_far
+"""
 
 #TODO: create function, write tests, backwards compatible/version agnostic
     
-counter = 0
-total = 0
 smallest_so_far = None
 largest_so_far = None
 userinp = None
@@ -17,12 +38,11 @@ while True:
         if userinp == 'done':
             break
         number = int(userinp)
-        counter = counter + 1
-        total = total + number
         if smallest_so_far == None or number < smallest_so_far:
             smallest_so_far = number
         if largest_so_far == None or number > largest_so_far:
             largest_so_far = number
     except:
-        print ('bad data')
-print (total,counter,smallest_so_far,largest_so_far)
+        print ('Invalid input')
+print ('Maximum is:',largest_so_far)
+print ('Minimum is:', smallest_so_far)
