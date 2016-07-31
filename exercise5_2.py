353d1120c76bbbae475c04a6b4d5f36fb30b5fff
Luciano Rubio <luciano@loociano.com>
2016-07-31 20:45:53 +0100

    Remove python2 in exercise 5.2

--- exercise5_2.py
+++ exercise5_2.py
@@ -6,29 +6,8 @@ number catch it with a try/except and put out an appropriate message and
 ignore the number. Enter the numbers from the book for problem 5.1 and Match
 the desired output as shown"""
 
-"""
-PYTHONv2
-smallest_so_far = None
-largest_so_far = None
-userinp = None
-while True:
-    userinp = raw_input('Enter a number: ')
-    try:
-        if userinp == 'done':
-            break
-        number = int(userinp)
-        if smallest_so_far == None or number < smallest_so_far:
-            smallest_so_far = number
-        if largest_so_far == None or number > largest_so_far:
-            largest_so_far = number
-    except:
-        print 'Invalid input'
-print 'Maximum is',largest_so_far
-print 'Minimum is', smallest_so_far
-"""
-
 #TODO: create function, write tests, backwards compatible/version agnostic
-    
+
 smallest_so_far = None
 largest_so_far = None
 userinp = None
